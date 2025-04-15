9f0d2fb5a5091c28b267bba324ac6ca7fa43f044
elisechristiann <elise.christiann@gmail.com>
2025-04-15 17:10:57 -0600

    updatesss

--- Project/notes_4_1.R
+++ Project/notes_4_1.R
@@ -1,6 +1,37 @@
-install.packages('rentrez')
 library(rentrez)
+library(tidyverse)
+library(ape)
+library(phangorn)
+library(ggtree)
 
-entrez_dbs()
+vignette("Trees", package="phangorn")
+
+#install ape
+#install phytools
+#phangorn
+#install DECIPHER
+#install ggtree
+
 
 entrez_db_summary("protein")
+
+dat <- read_csv("phi-base_current.csv")
+
+View(dat)
+
+#removing the first row
+
+#need to pull out the rip ones or whatever can make a tree
+dat1 <- 
+  dat[-1,] %>% 
+  filter(Pathogen_species=="Ralstonia solanacearum") %>%
+  filter(!is.na(GeneLocusID)) %>% filter(!is.na(Disease_name)) %>% filter(!is.na(Experimental_strain)) %>%
+  filter(grepl("^rip", Gene_name, ignore.case = TRUE)) %>%
+  select("ProteinID","GeneLocusID","Gene_name","Pathogen_species","Disease_name","Phenotype_of_mutant","Experimental_host_species","Host_descripton", "Experimental_strain")
+  
+View(dat1)
+#select ProtienID, GeneLocusID, Genomic Sequence Providing Strain, Gene_name, Disease_name, 
+#raldat <- dat1 %>%
+ # filter(Pathogen_species=="Ralstonia solanacearum") %>%
+#  pivot_longer()
+#View(raldat)
